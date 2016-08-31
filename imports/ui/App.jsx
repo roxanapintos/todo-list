bd26f63717f4e0e810f33cb694124a89491021ce
Roxana Pintos <roxypin@ipcorp.com.ar>
2016-08-31 12:46:36 -0300

    add form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,28 @@
 import React, { Component, PropTypes } from 'react';
-
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Tasks } from '../api/task.js';
  
 import Task from './Task.jsx';
  
-// App component - represents the whole app
-
+// app component - represents the whole app
 class App extends Component {
+  /**/
+  handleSubmit (event) {
+    event.preventDefault();
+
+    // find the text field via the React ref
+    const text = ReactDOM.findDOMNode (this.refs.textInput).value.trim();
+
+    Tasks.insert ({
+      text,
+      createdAt: new Date(), //current time
+    });
+
+    // clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value ='';    
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -19,6 +34,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            {/*handleSubmit in React listen to browser events*/}
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Add new tasks"
+            />
+          </form>
         </header>
  
         <ul>
